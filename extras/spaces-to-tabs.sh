5cadd89f82e974e21c7e5d7bc190b155fc2b8646
Jake Stine <Jake.Stine@gmail.com>
2018-01-05 08:25:53 -0800

    Added CLI options --edit-as-tabs, --edit-as-spaces, --edit-as-is, and --help

--- extras/spaces-to-tabs.sh
+++ extras/spaces-to-tabs.sh
@@ -2,26 +2,26 @@
 
 me=$(basename "$0")
 if [[ -z "$1" || "$1" == "--help" ]]; then
-	echo "Converts all spaces to tabs in a given directory."
-	echo "Modifies *.cpp;*.h;*.inl;*.lua (tabsize=4)"
-	echo "	ex:	 $ $me [search_dir]"
-	exit -1
+    echo "Converts all spaces to tabs in a given directory."
+    echo "Modifies *.cpp;*.h;*.inl;*.lua (tabsize=4)"
+    echo "  ex:  $ $me [search_dir]"
+    exit -1
 fi
 
 
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
 filetypes=(
-	"-name '*.cpp' "
-	"-name '*.h'   "
-	"-name '*.inl' "
-	"-name '*.lua' "
-	"-name '*.sh'  "
-	"-name '*.fx'  "
+    "-name '*.cpp' "
+    "-name '*.h'   "
+    "-name '*.inl' "
+    "-name '*.lua' "
+    "-name '*.sh'  "
+    "-name '*.fx'  "
 )
 
 excludes=(
     "-path '*imports/*'"
 )
 
-find . -type f \( $(join_by "-o " ${filetypes[@]} \) -not \( $(join_by "-o " ${excludes[@]} \) -exec bash -c 'unexpand -a -t 4 "$0" > /tmp/e && mv	/tmp/e "$0"' {} \;
+find . -type f \( $(join_by "-o " ${filetypes[@]} \) -not \( $(join_by "-o " ${excludes[@]} \) -exec bash -c 'unexpand -a -t 4 "$0" > /tmp/e && mv  /tmp/e "$0"' {} \;
--- git-filter-tab-install.sh
+++ git-filter-tab-install.sh
@@ -1,78 +1,138 @@
 #!/bin/bash
 
-# TODO: add proper commandline support
-#	-t	  to specify tab size override
-#	First naked (non-switch) parameter should be the git repository base dir [optional]
+FILTER_MODE=help
+SHOW_HELP=0
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
 
-ts=${1:-4}
+case $key in
+    -t|--tabsize)
+    ts="$2"
+    shift 2 # past argument + value
+    ;;
+    --filter-input|--edit-as-spaces)
+    FILTER_MODE="input"
+    shift
+    ;;
+    --filter-all|--edit-as-tabs)
+    FILTER_MODE="all"
+    shift
+    ;;
+    --filter-none|--edit-as-is)
+    FILTER_MODE="none"
+    shift
+    ;;
+    --help)
+    SHOW_HELP=1
+    shift
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
 
-rootpath=$(pwd)
+me=$(basename "$0")
+
+if [[ "$SHOW_HELP" -eq "1" || "$FILTER_MODE" == "help" ]]; then
+    echo "$me - Sets TabSpace Filtering Rules for GIT"
+    echo
+    echo "To edit locally as tabs and convert to spaces upstream :"
+    echo "  $ $me --tabsize=4 --edit-as-tabs {repository_path}"
+    echo
+    echo "To edit locally as spaces and convert errant tabs to spaces upstream:"
+    echo "  $ $me --tabsize=4 --edit-as-spaces {repository_path}"
+    echo
+    echo "To disable the filter and restore default  behavior:"
+    echo "  $ $me --edit-as-is {repository_path}"
+    echo
+    echo '  `repository_path` is optional.  If not specified, the GIT repository associated'
+    echo '  with the CWD is used.'
+    echo
+    
+    exit 1
+fi
+
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+rootpath=${1:-$(pwd)}
 if [[ "$rootpath" == */.git/* || "$rootpath" == */.git ]]; then
-	while [[ "$rootpath" == */.git/* || "$rootpath" == */.git ]]; do
-		rootpath=$(dirname "$rootpath"); 
-	done
-	cd "$rootpath"
+    while [[ "$rootpath" == */.git/* || "$rootpath" == */.git ]]; do
+        rootpath=$(dirname "$rootpath"); 
+    done
+    cd "$rootpath"
 fi
 
 gitpath="$(git rev-parse --show-toplevel)"
 
 if [[ -z "$gitpath" ]]; then
-	>&2 echo "ERROR: CWD is not a valid or recognized GIT directory."
-	exit 1
+    >&2 echo "ERROR: CWD is not a valid or recognized GIT directory."
+    exit 1
 fi
 
-echo "Installing TAB character filter @ $gitpath"
+echo "Applying changed to clone @ $gitpath"
 
 gitpath="$(readlink -f "$gitpath")"
 gitpath="$gitpath/.git"
 
-printf "Registering filter.tabstyle with tabsize=$ts... "
-git config --local filter.tabstyle.clean  "expand --tabs=$ts"	   || exit -1
-git config --local filter.tabstyle.smudge "unexpand -a --tabs=$ts" || exit -1
-printf "DONE!\n"
+if [[ "$FILTER_MODE" == "none"  ]]; then
+    printf "Disabling filter.autoexpandtabs... "
+    git config --local filter.autoexpandtabs.clean  ""                       || exit -1
+    git config --local filter.autoexpandtabs.smudge ""                       || exit -1
+    printf "DONE!\n"
+fi
 
-# ----------------------
-# this snippet behaves like core.auto_crlf=input, and should be recommended for any
-# developer who is not keen on using tab characters.
-#  * Check out as-is, commit as spaces.
-#
-# (todo: make this accessible via CLI)
+if [[ "$FILTER_MODE" == "all"   ]]; then
+    printf "Registering filter.autoexpandtabs with tabsize=$ts... "
+    git config --local filter.autoexpandtabs.clean  "expand --tabs=$ts"      || exit -1
+    git config --local filter.autoexpandtabs.smudge "unexpand -a --tabs=$ts" || exit -1
+    printf "DONE!\n"
+fi
 
-#printf "Registering filter.spacestyle with tabsize=$ts... "
-#git config --local filter.spacestyle.clean  "expand --tabs=$ts"   || exit -1
-#git config --local filter.spacestyle.smudge ""                    || exit -1
-#printf "DONE!\n"
-# ----------------------
+if   [[ "$FILTER_MODE" == "input" ]]; then
+    printf "Registering filter.autoexpandtabs with tabsize=$ts... "
+    git config --local filter.autoexpandtabs.clean  "expand --tabs=$ts"      || exit -1
+    git config --local filter.autoexpandtabs.smudge ""                       || exit -1
+    printf "DONE!\n"
+fi
 
 attrib_file="$gitpath/info/attributes"
 
 filetypes=(
-	"*.py"
-	"*.c"
-	"*.cpp"
-	"*.h"
-	"*.hpp"
-	"*.inl"
-	"*.sh"
-	"*.lua"
-	"*.bat"
-	"*.cmd"
+    "*.py"
+    "*.c"
+    "*.cpp"
+    "*.h"
+    "*.hpp"
+    "*.inl"
+    "*.sh"
+    "*.lua"
+    "*.bat"
+    "*.cmd"
 )
+
+# when uninstalling filter, don't bother adding/updating the attributes file.
+if [[ "$FILTER_MODE" == "none"  ]]; then
+        exit 0
+fi
 
 echo "Updating $(readlink -f "$attrib_file")..."
 
 for ft in ${filetypes[@]}; do
-	# check if entry for this file type already exists, if so then do not append duplicate.
-	# this works nicely to allow a user to comment out lines manually in their attributes file,
-	# and this script won't modify or override that preference.
-	
-	printf "	%-8s   --> " "$ft"
-	if grep "$ft"  "$attrib_file" | grep "tabspace" > /dev/null; then
-		echo "SKIPPED"
-	else
-		printf "%-8s   filter=tabspace\n" "$ft"	 >> "$attrib_file"
-		echo "ADDED"
-	fi
+    # check if entry for this file type already exists, if so then do not append duplicate.
+    # this works nicely to allow a user to comment out lines manually in their attributes file,
+    # and this script won't modify or override that preference.
+    
+    printf "    %-8s   --> " "$ft"
+    if grep "$ft"  "$attrib_file" | grep "autoexpandtabs" > /dev/null 2>/dev/null; then
+        echo "SKIPPED"
+    else
+        printf "%-8s   filter=autoexpandtabs\n" "$ft"  >> "$attrib_file"
+        echo "ADDED"
+    fi
 done
 
 echo "(note: any skipped entries already exist in attributes file)"
