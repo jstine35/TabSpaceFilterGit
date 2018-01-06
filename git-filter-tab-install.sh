52ed2901d5ee66ad457045c64ac10dac850aff88
Jake Stine <Jake.Stine@gmail.com>
2018-01-06 06:41:16 -0800

    Added support for side-by-side filtering as spaces or tabs.
     * added more types to the filter list: C#, visual studio project files, xml.
     * added section identifier to git attributes file
     * --edit-as-is (uninstall) now removes that section entirely, rather than leaving it stale

--- git-filter-tab-install.sh
+++ git-filter-tab-install.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 
+# list of file types on which to apply tabspace filtering.
+filetypes_as_tabs=(
+    "*.py"
+    "*.c"
+    "*.cpp"
+    "*.cs"
+    "*.h"
+    "*.hpp"
+    "*.inl"
+    "*.sh"
+    "*.lua"
+    "*.bat"
+    "*.cmd"
+    "*.md"
+    "*.txt"
+)
+
+# following types are edited as spaces locally, even if --edit-as-tabs has been
+# specified.  These types are usually  tabspaced as 2 spaces, and editing them 
+# as tabs is not helpful to anyone.
+
+filetypes_as_spaces=(
+    "*.csproj"
+    "*.props"
+    "*.vcxproj"
+    "*.xml"
+)
+
 FILTER_MODE=help
 SHOW_HELP=0
+ts=4
+
 POSITIONAL=()
 while [[ $# -gt 0 ]]
 do
@@ -66,7 +96,16 @@ if [[ "$rootpath" == */.git/* || "$rootpath" == */.git ]]; then
     cd "$rootpath"
 fi
 
-gitpath="$(git rev-parse --show-toplevel)"
+gitpath=$(git rev-parse --show-toplevel)
+
+# rev-parse returns a malformed filename on Windows OS, which contains both a drive:colon
+# specifier and forward slashes, eg c:/some/path -- this confuses msys into not auto-correcting
+# the path to the expected bash format of /c/some/path.  So we must do so manually via cygpath,
+# elsewise some of our later commands like grep might fail to find the file...
+
+if where cygpath; then
+    gitpath=$(cygpath "$gitpath")
+fi
 
 if [[ -z "$gitpath" ]]; then
     >&2 echo "ERROR: CWD is not a valid or recognized GIT directory."
@@ -78,61 +117,62 @@ echo "Applying changed to clone @ $gitpath"
 gitpath="$(readlink -f "$gitpath")"
 gitpath="$gitpath/.git"
 
-if [[ "$FILTER_MODE" == "none"  ]]; then
-    printf "Disabling filter.autoexpandtabs... "
-    git config --local filter.autoexpandtabs.clean  ""                       || exit -1
-    git config --local filter.autoexpandtabs.smudge ""                       || exit -1
-    printf "DONE!\n"
-fi
+if   [[ "$FILTER_MODE" != "none" ]]; then
+    printf "Registering filters with tabsize=$ts... "
+    git config --local  filter.editastabs.clean   "expand --tabs=$ts"       || exit -1
+    git config --local  filter.editastabs.smudge  "unexpand -a --tabs=$ts"  || exit -1
 
-if [[ "$FILTER_MODE" == "all"   ]]; then
-    printf "Registering filter.autoexpandtabs with tabsize=$ts... "
-    git config --local filter.autoexpandtabs.clean  "expand --tabs=$ts"      || exit -1
-    git config --local filter.autoexpandtabs.smudge "unexpand -a --tabs=$ts" || exit -1
-    printf "DONE!\n"
-fi
-
-if   [[ "$FILTER_MODE" == "input" ]]; then
-    printf "Registering filter.autoexpandtabs with tabsize=$ts... "
-    git config --local filter.autoexpandtabs.clean  "expand --tabs=$ts"      || exit -1
-    git config --local filter.autoexpandtabs.smudge ""                       || exit -1
+    git config --local  filter.editasspaces.clean  "expand --tabs=$ts"      || exit -1
+    git config --local  filter.editasspaces.smudge ""                       || exit -1
     printf "DONE!\n"
 fi
 
 attrib_file="$gitpath/info/attributes"
 
-filetypes=(
-    "*.py"
-    "*.c"
-    "*.cpp"
-    "*.h"
-    "*.hpp"
-    "*.inl"
-    "*.sh"
-    "*.lua"
-    "*.bat"
-    "*.cmd"
-)
-
-# when uninstalling filter, don't bother adding/updating the attributes file.
-if [[ "$FILTER_MODE" == "none"  ]]; then
-        exit 0
-fi
-
 echo "Updating $(readlink -f "$attrib_file")..."
 
-for ft in ${filetypes[@]}; do
-    # check if entry for this file type already exists, if so then do not append duplicate.
-    # this works nicely to allow a user to comment out lines manually in their attributes file,
-    # and this script won't modify or override that preference.
-    
-    printf "    %-8s   --> " "$ft"
-    if grep "$ft"  "$attrib_file" | grep "autoexpandtabs" > /dev/null 2>/dev/null; then
-        echo "SKIPPED"
-    else
-        printf "%-8s   filter=autoexpandtabs\n" "$ft"  >> "$attrib_file"
-        echo "ADDED"
-    fi
-done
+# Remove existing entries, stop after one match.
+line_first=$(grep -m1 -n "# FilterTab Section:BEGIN" "$attrib_file" | cut -d':' -f1)
+line_last=$(grep  -m1 -n "# FilterTab Section:END"   "$attrib_file" | cut -d':' -f1)
 
-echo "(note: any skipped entries already exist in attributes file)"
+if [[ -n "$line_first" && -n "$line_last" ]]; then
+    echo "    Deleting existing FilterTab Section found at lines $line_first -> $line_last"
+    sed -i -e "${line_first},${line_last}d" "$attrib_file"
+fi
+
+declare -a filetypes
+
+add_filetypes_to_attributes_file() {
+    filtername="$1"
+    for ft in "${filetypes[@]}"; do
+        # check if entry for this file type already exists, if so then do not append duplicate.
+        # this works nicely to allow a user to comment out lines manually in their attributes file,
+        # and this script won't modify or override that preference.
+        
+        printf "    %-12s   --> " "$ft"
+        if grep "$ft"  "$attrib_file" | grep "$filtername" > /dev/null 2>/dev/null; then
+            echo "SKIPPED"
+        else
+            >> "$attrib_file" printf "%-12s  filter=$filtername\n" "$ft"
+            echo "ADDED as filter=$filtername"
+        fi
+    done
+}
+
+if [[ "$FILTER_MODE" != "none" ]]; then
+    >> "$attrib_file" echo "# FilterTab Section:BEGIN"
+    >> "$attrib_file" echo "# warning: git-filter-tab-install will overwrite any direct modifications to this section."
+
+    filetypes=("${filetypes_as_tabs[@]}")
+    tabulars=$([ "$FILTER_MODE" == "all" ] && echo "editastabs" || echo "editasspaces")
+    add_filetypes_to_attributes_file "$tabulars"
+    
+    filetypes=("${filetypes_as_spaces[@]}")
+    add_filetypes_to_attributes_file "editasspaces"
+
+    >> "$attrib_file" echo "# FilterTab Section:END"
+    echo "(note: any skipped entries already exist in attributes file)"
+
+else
+    echo "FilterTab attributes removed.  Uninstall complete."
+fi
