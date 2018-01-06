6f567cdddd410f76e9c2732b2b7a6a26f364a626
Jake Stine <Jake.Stine@gmail.com>
2018-01-06 12:09:41 -0800

    Fixed mismatched whitespace

--- extras/tabs-to-spaces.sh
+++ extras/tabs-to-spaces.sh
@@ -2,25 +2,25 @@
 
 me=$(basename "$0")
 if [[ -z "$1" || "$1" == "--help" ]]; then
-	echo "Converts all tabs to spaces in a given directory."
-	echo "Modifies *.cpp;*.h;*.inl;*.lua (tabsize=4)"
-	echo "	ex:	 $ $me [search_dir]"
-	exit -1
+    echo "Converts all tabs to spaces in a given directory."
+    echo "Modifies *.cpp;*.h;*.inl;*.lua (tabsize=4)"
+    echo "  ex:  $ $me [search_dir]"
+    exit -1
 fi
 
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
 filetypes=(
-	"-name '*.cpp' "
-	"-name '*.h'   "
-	"-name '*.inl' "
-	"-name '*.lua' "
-	"-name '*.sh'  "
-	"-name '*.fx'  "
+    "-name '*.cpp' "
+    "-name '*.h'   "
+    "-name '*.inl' "
+    "-name '*.lua' "
+    "-name '*.sh'  "
+    "-name '*.fx'  "
 )
 
 excludes=(
     "-path '*imports/*'"
 )
 
-find . -type f \( $(join_by "-o " ${filetypes[@]} \) -not \( $(join_by "-o " ${excludes[@]} \) -exec bash -c 'expand -t 4 "$0" > /tmp/e && mv	/tmp/e "$0"' {} \;
+find . -type f \( $(join_by "-o " ${filetypes[@]} \) -not \( $(join_by "-o " ${excludes[@]} \) -exec bash -c 'expand -t 4 "$0" > /tmp/e && mv   /tmp/e "$0"' {} \;
